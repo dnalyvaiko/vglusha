4a44aeb89368e293d16dc887ea97915b5492a9e0
dnalyvaiko <nalivayko_d@mail.ru>
2016-07-01 18:04:16 +0300

    Add gulp cssmin, rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename');
 
 gulp.task('sass', function(){
     return gulp.src('app/sass/**/*.scss')
@@ -18,7 +22,24 @@ gulp.task('browser-sync', function(){
     });
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function(){
+gulp.task('scripts', function(){
+  return gulp.src([
+    'app/libs/jquery/dist/jquery.min.js',
+    'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-libs', ['sass'], function(){
+  return gulp.src('app/css/libs.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function(){
   gulp.watch('app/sass/**/*.sass', ['sass']);
   gulp.watch('app/*html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
