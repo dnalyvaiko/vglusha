841030d736ccd9229eb69c9ea0d87dbbc8549770
dnalyvaiko <nalivayko_d@mail.ru>
2016-07-03 13:04:39 +0300

    Add build plugin

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglifyjs'),
     cssnano = require('gulp-cssnano'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    del = require('del');
 
 gulp.task('sass', function(){
     return gulp.src('app/sass/**/*.scss')
@@ -43,4 +44,27 @@ gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function(){
   gulp.watch('app/sass/**/*.sass', ['sass']);
   gulp.watch('app/*html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('clean', function(){
+  return del.synk('dist');
+})
+
+gulp.task('build', ['sass', 'scripts'], function(){
+
+  var buildCss = gulp.src([
+      'app/css/main.css',
+      'app/css/libs.min.css'
+      ])
+  .pipe(gulp.dest('dist/css'))
+
+  var buildFonts = gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+
+  var buildJs = gulp.src('app/js/**/*')
+  .pipe(gulp.dest('dist/js'))
+
+  var buildHtml = gulp.src('app/*.html')
+  .pipe(gulp.dest('dist'));
+
 });
